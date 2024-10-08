61c1473c870c7eea9004e86fdab6c0307b4c2f05
Axel W <axelwestman@outlook.com>
2024-10-08 12:26:00 -0300

    arreglo en archivo php

--- pages/formulario.php
+++ pages/formulario.php
@@ -12,7 +12,7 @@ $nombre = $_POST["name"] ;
 $email = $_POST["email"] ;
 $comentario = $_POST["text"] ;
 
-//verificamos la conexion a base datos
+//verificamos la conexion a base datos y enviamos los datos
 if(!$connection) 
         {
             echo ("No se ha podido conectar con el servidor" . mysqli_conncect_error());
@@ -21,18 +21,15 @@ if(!$connection)
         {
             echo "<b><h3>Hemos conectado al servidor</h3></b>" ;
             $datab = "legend_of_elda";
-        //indicamos selecionar ala base datos
-        $db = mysqli_select_db($connection,$datab);
+            //indicamos selecionar ala base datos
+             $db = mysqli_select_db($connection,$datab);
 
-        //insertamos datos de registro al mysql xamp, indicando nombre de la tabla y sus atributos
-        $instruccion_SQL = "INSERT INTO sugerencias (nombre, email, comentario)
+            //insertamos datos de registro al mysql xamp, indicando nombre de la tabla y sus atributos
+            $instruccion_SQL = "INSERT INTO sugerencias (nombre, email, comentario)
                              VALUES ('$nombre','$email','$comentario')";
                            
                             
-        $resultado = mysqli_query($connection,$instruccion_SQL);
-        header("Location:./sugerencias.html");
+            $resultado = mysqli_query($connection,$instruccion_SQL);
+            header("Location:./sugerencias.html");
         }
-        
-
-
 ?>
